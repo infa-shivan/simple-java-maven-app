edf8822935d2c1d7ea2ee2a85a0543f6a82a1936
Pa1sathvik <7vik.sathvik@gmail.com>
2021-05-20 14:54:33 +0530

    Vulnerable
    Vulnerable

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,31 +1,48 @@
 package com.mycompany.app;
 
+import java.io.*;
+import java.io.FileReader;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  */
-public class App
-{
+public class App {
 
-    private final String message = "Hello World!";
+	private static Logger log = Logger.getLogger(App.class.getName());
+	private final String message = "Hello World!";
+	private final String PASSWORD = "Hello World!";
 
-    public App() {}
-	
-	
+	public App() {
+	}
+
 	public static void foo(long l) {
 
-             for(int i=0; i<l; i++) {
-                      // do something
-             }
-     }
+		for (int i = 0; i < l; i++) {
+			// do something
+		}
+	}
 
-    
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        // do some thing....
-    }
+		String s = "Sathvik";
+		String g = "Sathvik";
 
-    private final String getMessage() {
-        return message;
-    }
+		System.out.println(s == g);
+
+		String val = "vulnn";
+		try {
+
+			int value = Integer.parseInt(val);
+		} catch (NumberFormatException e) {
+			log.info("Failed to parse val = " + val);
+		}
+		
+		System.exit(1);
+
+	}
+
+	private final String getMessage() {
+		return message;
+	}
 }
-
